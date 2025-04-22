9803781d5e1121738d23d747428d5692502029c4
Nazrina <nazrinakhan88@gmail.com>
2025-04-21 20:26:43 -0400

    igw

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -61,6 +61,11 @@ resource "aws_route_table" "public" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
 
   tags = {
     Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
@@ -105,10 +110,6 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.*.id[count.index]
 }
 
-route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
 
 
 resource "aws_route_table_association" "web" {
