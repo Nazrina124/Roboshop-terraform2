9d90ad892172b99d3b79d1f1360f0d7b85744281
Nazrina <nazrinakhan88@gmail.com>
2025-04-22 21:38:36 -0400

    creating security group

--- module/ec2/variables.tf
+++ module/ec2/variables.tf
@@ -1,10 +1,10 @@
 
 
     variable "name" {}
-    varibale "instance_type" {}
-    varibale "allow_port" {}
-    varible "allow_sg_cidr" {}
-    varibale "subnets" {}
-    varibale "vpc_id" {}
+    variable "instance_type" {}
+    variable "allow_port" {}
+    variable "allow_sg_cidr" {}
+    variable "subnets" {}
+    variable "vpc_id" {}
     variable "env" {}
     variable "bastion_nodes" {}
