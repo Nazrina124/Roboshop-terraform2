a50e5dc0ee57feadf1bb4aff13e9a49b45527e71
Nazrina <nazrinakhan88@gmail.com>
2025-04-24 16:06:58 -0400

    ec2

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -34,7 +34,7 @@ ingress {
 
 resource "aws_launch_template" "main" {
     name = "${var.name}-${var.env}-lt"
-    image_id = data.aws_ami.rhel9.image.id
+    image_id = data.aws_ami.rhel9.image_id
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
