2c0495da5b26fdf0e166c35b2ce420e20b99facd
Nazrina <nazrinakhan88@gmail.com>
2025-04-25 13:27:48 -0400

    ec2

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -66,7 +66,7 @@ resource "aws_autoscaling_group" "main" {
 }
 
 resource "aws_instance" "main" {
-    ami = "data.aws_ami.rhel9.image_id"
+    image_id = data.aws_ami.rhel9.image_id
     instance_type = var.instance_type
     subnet_id = var.subnets_ids[0]
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
