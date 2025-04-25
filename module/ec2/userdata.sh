9c6c78f809fdd0a5cd607c5943a557a54f97ee0f
Nazrina <nazrinakhan88@gmail.com>
2025-04-25 15:23:45 -0400

    ec2

--- module/ec2/userdata.sh
+++ module/ec2/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip3.11 install ansible hvac
-ansible-pull -i localhost, -U https://github.com/Nazrina124/Ansible-Robo2 main.yml -e env=${env} -e role_name=${role_name} main.yml  -e vault_token=${VAULT_TOKEN} 2>&1 | tee -a  /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Nazrina124/Ansible-Robo2 main.yml -e env=${env} -e role_name=${role_name} main.yml  -e vault_token=${VAULT_TOKEN} 2>&1 | tee -a /opt/userdata.log
 
