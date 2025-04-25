f8f7393dee4594d7aaf56195f59c23a4562a8109
Nazrina <nazrinakhan88@gmail.com>
2025-04-25 13:59:01 -0400

    ec2

--- module/ec2/userdata.sh
+++ module/ec2/userdata.sh
@@ -1,3 +1,3 @@
 pip3.11 install ansible
-ansible-pull -i localhost, -U https://github.com/Nazrina124/Ansible-Robo2 main.yml -e role_name=role_name main.yml -e vault_token=Vault_token | 2>&1 /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Nazrina124/Ansible-Robo2 main.yml -e role_name=role_name main.yml -e vault_token=Vault_token 2>&1 | tee -a  /opt/userdata.log
 
