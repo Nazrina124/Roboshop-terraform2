38756dd9c85696276e3ce39c8391042c22bfaf64
Nazrina <nazrinakhan88@gmail.com>
2025-04-26 09:54:17 -0400

    creating dns record

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -97,7 +97,7 @@ tags = {
 resource "aws_route53_record" "instance" {
   count = var.asg ? 0 : 1
   zone_id = var.zone_id
-  name    = "${var.name}.${var.env}-sg"
+  name    = "${var.name}.${var.env}"
   type    = "A"
   ttl     = 10
   records = [aws_instance.main.*.private_ip[count.index]]
