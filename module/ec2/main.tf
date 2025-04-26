7b0c43293a028a260387539893ba5b0be9e55fc1
Nazrina <nazrinakhan88@gmail.com>
2025-04-26 18:56:45 -0400

    ec2

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -163,20 +163,6 @@ resource "aws_lb_target_group" "main" {
 
 
 
-###### Redirect Action
-
-resource "aws_lb_listener" "front_end" {
-  count             = var.asg ? 1 : 0
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.*.arn[count.index]
-  }
-}
-
 
 
 
