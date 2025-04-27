f91bce4df7f3a14524830cefb47adf84113d2a57
Nazrina <nazrinakhan88@gmail.com>
2025-04-27 16:04:11 -0400

    asg module

--- module/asg/main.tf
+++ module/asg/main.tf
@@ -91,6 +91,14 @@ egress {
   }
 
 ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "TCP"
+    cidr_blocks      = var.allow_lb_sg_cidr
+
+  }
+
+ingress {
     from_port        = 443
     to_port          = 443
     protocol         = "TCP"
