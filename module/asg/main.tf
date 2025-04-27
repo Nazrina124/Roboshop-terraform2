43cab7360f32a61fbb0cfb829d7f555d468a9e74
Nazrina <nazrinakhan88@gmail.com>
2025-04-27 16:07:11 -0400

    update certificate

--- module/asg/main.tf
+++ module/asg/main.tf
@@ -176,6 +176,23 @@ resource "aws_lb_listener" "public-http" {
  }
 }
 
+resource "aws_lb_listener" "public-http" {
+   count             = var.internal ? 0 : 1   ###### if app is external create https
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 ####DNS record for frontend for load balancer
 
 resource "aws_route53_record" "lb" {
