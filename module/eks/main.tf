1fb20003fd45f91ba785f0e89942fb7cb6ab051c
Nazrina <nazrinakhan88@gmail.com>
2025-05-12 19:42:57 -0400

    eks

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "main" {
   name = "${var.env}-eks"
-  role_arn = aws_iam_role.cluster.arn
+  role_arn = aws_iam_role.eks-cluster.arn
   version =  var.eks_version
   
   vpc_config {
@@ -25,9 +25,3 @@ resource "aws_eks_cluster" "main" {
 
  }
 
- resource "aws_eks_addon" "example" {
-  cluster_name                = aws_eks_cluster.example.name
-  addon_name                  = "coredns"
-  addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
-  resolve_conflicts_on_update = "PRESERVE"
-}
