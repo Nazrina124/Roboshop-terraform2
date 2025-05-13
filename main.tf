792c01a0834e690cadc99cc69bbd93b963fd4b7b
Nazrina <nazrinakhan88@gmail.com>
2025-05-12 20:28:17 -0400

    eks module

--- main.tf
+++ main.tf
@@ -43,4 +43,6 @@ module "eks" {
 
 
 #### Command to config cluster aws eks update-kubeconfig --name dev-eks
+
+### additional githubrunner access for cluster. hence we have to add access_config
    
